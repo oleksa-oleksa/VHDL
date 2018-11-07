310d9d260a7aeeac5f01ceb1d4c6c8480ee674ab
Oleksandra Baga <oleksandra.baga@gmail.com>
2018-11-07 13:19:32 +0100

    SIPO simulation done

--- Electronic_Design_Automation/Lab01_BasicComponents/shift_sipo.vhd
+++ Electronic_Design_Automation/Lab01_BasicComponents/shift_sipo.vhd
@@ -18,11 +18,11 @@ architecture Behav of shift_sipo is
 
 signal s_qo : STD_LOGIC_VECTOR(3 downto 0) := "UUUU";
 signal s_q3, s_q2, s_q1, s_q0 : STD_LOGIC := 'U';
-signal rd : std_logic := '0';
 
 begin
-    sipo : process (clk, reset, rd) is   
-    variable counter : unsigned ( 1 DOWNTO 0 ) := "00";                  
+    sipo : process (clk, reset) is   
+    variable counter : unsigned ( 1 DOWNTO 0 ) := "00";
+    variable rd : std_logic := '0';                  
     begin
 	
         if (reset = '1') then
@@ -31,7 +31,7 @@ begin
 	    s_qo(2) <= '0';
 	    s_qo(1) <= '0';
 	    s_qo(0) <= '0';
-	    rd <= '0';
+	    rd := '0';
 
 	 elsif (falling_edge(clk) AND rd = '0' AND reset = '0') then
 	    s_q0 <= din;
@@ -40,23 +40,30 @@ begin
 	    s_q3 <= s_q2;
 
 	    if (counter = "11") then
-  		rd <= '1';
+  		rd := '1';
 	    end if;
  	    
             counter := counter + 1;
 
 	 elsif (falling_edge(clk) AND rd = '1' AND reset = '0') then
 	    -- cleaning for a new cycle
-	    if (counter = "11") then
-	    	counter := "00"; 
-	    	rd <= '0';
+	    if (counter = "00") then 
+	    	rd := '0';
        	    end if;
+	
+	  -- loading
+	    s_q0 <= din;
+	    s_q1 <= s_q0;
+	    s_q2 <= s_q1;
+	    s_q3 <= s_q2;
+
            -- output
 	    s_qo(3) <= s_q3;
 	    s_qo(2) <= s_q2;
 	    s_qo(1) <= s_q1;
 	    s_qo(0) <= s_q0;
 	
+	   counter := counter + 1;
 	end if;      
 
     end process sipo;
